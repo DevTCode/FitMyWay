bd8154fea94436c049f05edfcb3baa1f3f4b1172
DevTCode <128856806+DevTCode@users.noreply.github.com>
2023-06-07 21:46:19 +0000

    Create user.dart

---
+++ healthy/lib/models/user.dart
@@ -0,0 +1,10 @@
+class User {
+  final String nom;
+  final String prenom;
+  final int age;
+  final double height;
+  final double weight;
+  final String sex;
+
+  User({required this.nom,required this.prenom,required this.height,required this.weight,required this.age,required this.sex});
+} 
